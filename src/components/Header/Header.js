bacb42e0dc679566d0dd3f69fbb84a6e6320a7bc
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-15 16:40:11 +0600

    hamburger added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
+  const [show, setShow] = useState(true);
+
   const handleToggleMenu = () => {
-    const floatingMenu = document.getElementById("floating-menu");
-    if (floatingMenu.style.display === "none") {
-      floatingMenu.style.display = "block";
-    } else if (floatingMenu.style.display === "block") {
-      floatingMenu.style.display = "none";
-    }
+    setShow(!show);
   };
+
   return (
     <div className="navbar-container">
       <div className="logo">
         <h2>The New Media News</h2>
       </div>
       <div className="menu">
-        <ul id="floating-menu">
-          <li>Home</li>
-          <li>News</li>
-          <li>About</li>
-        </ul>
+        {show ? (
+          <ul>
+            <li>Home</li>
+            <li>News</li>
+            <li>About</li>
+          </ul>
+        ) : null}
       </div>
       <FontAwesomeIcon
         icon={faBars}
